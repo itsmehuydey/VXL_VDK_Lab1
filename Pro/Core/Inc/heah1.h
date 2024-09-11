ad0e47a65f08479895ebbf95c3ffc391c0309213
LAPTOP-NIOUD1BM\pc <huyt24309@gmail.com>
2024-09-11 20:29:10 +0700

    init

---
+++ Pro/Core/Inc/heah1.h
@@ -0,0 +1,13 @@
+/*
+ * heah1.h
+ *
+ *  Created on: Sep 11, 2024
+ *      Author: pc
+ */
+
+#ifndef INC_HEAH1_H_
+#define INC_HEAH1_H_
+
+
+
+#endif /* INC_HEAH1_H_ */
--- Pro/Core/Inc/main.h
+++ Pro/Core/Inc/main.h
@@ -58,6 +58,12 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define Led_Red_Pin GPIO_PIN_5
+#define Led_Red_GPIO_Port GPIOA
+#define Led_Yel_Pin GPIO_PIN_6
+#define Led_Yel_GPIO_Port GPIOA
+#define Led_Gree_Pin GPIO_PIN_7
+#define Led_Gree_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
