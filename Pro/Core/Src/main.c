fa021a9e66656b85312401d38d4c62e07ad437a4
LAPTOP-NIOUD1BM\pc <huyt24309@gmail.com>
2024-09-11 22:34:08 +0700

    EX2

--- Pro/Core/Src/main.c
+++ Pro/Core/Src/main.c
@@ -47,6 +47,7 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -83,17 +84,50 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-
+  int led_sta = 5;
+  int cnt = 0;
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+	 	  switch (led_sta) {
+	 	 	  case 5:
+	 	 		HAL_GPIO_WritePin(Led_Red_GPIO_Port, Led_Red_Pin, SET);
+	 	 		 HAL_GPIO_WritePin(Led_Yel_GPIO_Port, Led_Yel_Pin, RESET);
+	 	 		 HAL_GPIO_WritePin(Led_Green_GPIO_Port, Led_Green_Pin, RESET);
+	 	 		 if (cnt >= 5) {
+	 	 		      led_sta = 2;
+	 	 		      cnt = 0;
+	 	 		 }
+	 	 		  break;
+	 	 	  case 2:
+	 	 		 HAL_GPIO_WritePin(Led_Red_GPIO_Port, Led_Red_Pin, RESET);
+	 	 		 HAL_GPIO_WritePin(Led_Yel_GPIO_Port, Led_Yel_Pin, SET);
+	 	 		 HAL_GPIO_WritePin(Led_Green_GPIO_Port, Led_Green_Pin, RESET);
+	 	 		if (cnt >= 2) {
+	 	 			led_sta = 3;
+	 	 			cnt = 0;
+	 	 		}
+	 	 		  break;
+	 	 	  case 3:
+	 	 		  HAL_GPIO_WritePin(Led_Red_GPIO_Port, Led_Red_Pin, RESET);
+	 	 		  HAL_GPIO_WritePin(Led_Yel_GPIO_Port, Led_Yel_Pin, RESET);
+	 	 		 HAL_GPIO_WritePin(Led_Green_GPIO_Port, Led_Green_Pin, SET);
+	 	 		if (cnt >= 3) {
+	 	 			led_sta = 5;
+	 	 			cnt = 0;
+	 	 		}
+	 	 		  break;
+	 	 	  //default:
+	 	 		  //
+	 	 	  }
+	 	 	  HAL_Delay(1000);
+	 	 	  cnt++;
+	     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
 }
@@ -131,6 +165,30 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, Led_Red_Pin|Led_Yel_Pin|Led_Green_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : Led_Red_Pin Led_Yel_Pin Led_Green_Pin */
+  GPIO_InitStruct.Pin = Led_Red_Pin|Led_Yel_Pin|Led_Green_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
