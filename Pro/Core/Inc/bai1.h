992b5bd09acb5e351208d5a394e5c903e68717b6
LAPTOP-NIOUD1BM\pc <huyt24309@gmail.com>
2024-09-18 13:39:23 +0700

    no message

--- Pro/Core/Inc/bai1.h
+++ Pro/Core/Inc/bai1.h
@@ -4,10 +4,11 @@
  *  Created on: Sep 18, 2024
  *      Author: pc
  */
-#include "main.h"
+
 #ifndef INC_BAI1_H_
 #define INC_BAI1_H_
-void changeLed(int led_sta);
 
+#include "main.h"
+void changeLed(int led_sta);
 
 #endif /* INC_BAI1_H_ */
